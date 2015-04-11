f3443856bd76a5dff76aac45f42afb5d2a9c69ae
Alex Tonkonozhenko <tonkonozhenko@gmail.com>
2015-04-11 20:32:52 +0300

    Update brj.tuning.sh

--- brj.tuning.sh
+++ brj.tuning.sh
@@ -9,7 +9,7 @@ gsettings set org.pantheon.desktop.slingshot columns 4
 
 #gsettings set org.pantheon.desktop.gala.keybindings switch-input-source "['<Alt>Shift_L', '<Alt>Shift_R']"
 #gsettings set org.pantheon.desktop.gala.keybindings switch-input-source "['<Alt>Shift_L', '<Alt>Shift_R', '<Shift>Alt_L', '<Shift>Alt_R']"
-gsettings set org.pantheon.desktop.gala.keybindings switch-input-source "['<Ctrl>Shift_L', '<Ctrl>>Shift_R', '<Shift>Control_L', '<Shift>Control_R']"
+gsettings set org.pantheon.desktop.gala.keybindings switch-input-source "['<Ctrl>Shift_L', '<Ctrl>Shift_R', '<Shift>Control_L', '<Shift>Control_R']"
 
 gsettings set org.pantheon.desktop.wingpanel auto-adjust-alpha false
 gsettings set org.pantheon.desktop.wingpanel background-alpha 0.2
